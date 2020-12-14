d7eb57a90f39a55261dd59fbcb38461a3be12a6e
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-12-14 08:56:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 // Your code here
 class Cat{
+  constructor (name,sex){
+    this.name=name;
+    this.sex=sex;
+  }
   
+  speak(){
+    return `${this.name} says meow`;
+    
+  }
+}
+
+class Dog{
+   constructor (name,sex){
+    this.name=name;
+    this.sex=sex;
+  }
+  speak(){
+     return `${this.name} says woof`;
+    
+  }
   
 }
+
+class Bird{
+   constructor (name,sex){
+    this.name=name;
+    this.sex=sex;
+  }
+  speak(){
+    if (this.sex==='male'){
+      return `It's me! ${this.name}, the parrot!`;
+    }
+    else{
+      return `${this.name} says squawk!`;
+    }
+    
+  }
+  
+}
+
+const kat = new Cat(Kat,female);
+const max = new Dog(Max,male);
+const silv = new Bird(silv ,female);
+kat.speak();
+
+
